967f04eb34d9fffa50a2a4c52b6fca85fb18a9d2
Gayathri2803 <gayathribellapukonda28@gmail.com>
2025-07-12 12:58:57 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ echo "Stopping existing Node app..."
 pkill -f "node app.js" || true
 
 echo "Installing dependencies..."
-npm install
+npm install --omit=dev
 
 echo "Starting app in background..."
 nohup npm start > output.log 2>&1 &
